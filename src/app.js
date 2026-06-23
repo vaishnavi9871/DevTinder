16c17dbdb63691bdcb17ff48d7047c2cef0c6b09
vaishnavi9871 <vaishnaviagrahari9871@gmail.com>
2026-06-23 23:02:33 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
 const express=require('express');
 const app=express();
 
-//this will match all the HTTPS requests with the path /hello and send the response "Hello Vaishnavi Agrahari from JPL"
-app.use("/user",(req,res) => {
-    res.send("Hello Vaishnavi Agrahari from JPL");    
-});
 
-app.get("/user",(req,res) => {
-    res.send({firstname:"Vaishnavi",lastname:"Agrahari",age:22});
-});
-app.post("/user",(req,res) => {
-    res.send("User created successfully");
-} );  
-app.delete("/user",(req,res) => {           
-    res.send("User deleted successfully");
-});
-app.patch("/user",(req,res) => {
-    res.send("User updated successfully through patch");
-});
-app.put("/user",(req,res) => {
-    res.send("User updated successfully through put");
-});
+
+app.use("/user",(req,res,next) => {
+    // res.send("Route Handler 1");
+    next();
+    
+},
+(req,res,next) => {
+    // res.send("Route Handler 2");
+    next();
+} ,
+(req,res,next) => {
+    // res.send("Route Handler 2");
+    next();
+}   ,
+(req,res,next) => {
+    // res.send("Route Handler 2");
+    next();
+}     
+);
+
 
 app.listen(7777,()=>{
     console.log('Server is running on port 7777');
